a248ce2610bedde10d08cf34d9f8fb4553794f86
nobleknightt <60701544+nobleknightt@users.noreply.github.com>
2024-09-09 20:34:26 +0530

    added theme icon

--- src/themes.js
+++ src/themes.js
@@ -4,154 +4,176 @@ const themes = [
         backgroundColor: "bg-slate-50",
         backgroundColor_Tag: "bg-slate-100",
         backgroundColor_Card: "bg-slate-200",
-        textColor: "text-slate-950"
+        textColor: "text-slate-950",
+        hexCode: "#020617",
     },
     {
         name: "Gray",
         backgroundColor: "bg-gray-50",
         backgroundColor_Tag: "bg-gray-100",
         backgroundColor_Card: "bg-gray-200",
-        textColor: "text-gray-950"
+        textColor: "text-gray-950",
+        hexCode: "#030712",
     },
     {
         name: "Zinc",
         backgroundColor: "bg-zinc-50",
         backgroundColor_Tag: "bg-zinc-100",
         backgroundColor_Card: "bg-zinc-200",
-        textColor: "text-zinc-950"
+        textColor: "text-zinc-950",
+        hexCode: "#09090b",
     },
     {
         name: "Neutral",
         backgroundColor: "bg-neutral-50",
         backgroundColor_Tag: "bg-neutral-100",
         backgroundColor_Card: "bg-neutral-200",
-        textColor: "text-neutral-950"
+        textColor: "text-neutral-950",
+        hexCode: "#0a0a0a",
     },
     {
         name: "Stone",
         backgroundColor: "bg-stone-50",
         backgroundColor_Tag: "bg-stone-100",
         backgroundColor_Card: "bg-stone-200",
-        textColor: "text-stone-950"
+        textColor: "text-stone-950",
+        hexCode: "#0c0a09",
     },
     {
         name: "Red",
         backgroundColor: "bg-red-50",
         backgroundColor_Tag: "bg-red-100",
         backgroundColor_Card: "bg-red-200",
-        textColor: "text-red-950"
+        textColor: "text-red-950",
+        hexCode: "#450a0a",
     },
     {
         name: "Orange",
         backgroundColor: "bg-orange-50",
         backgroundColor_Tag: "bg-orange-100",
         backgroundColor_Card: "bg-orange-200",
-        textColor: "text-orange-950"
+        textColor: "text-orange-950",
+        hexCode: "#431407",
     },
     {
         name: "Amber",
         backgroundColor: "bg-amber-50",
         backgroundColor_Tag: "bg-amber-100",
         backgroundColor_Card: "bg-amber-200",
-        textColor: "text-amber-950"
+        textColor: "text-amber-950",
+        hexCode: "#451a03",
     },
     {
         name: "Yellow",
         backgroundColor: "bg-yellow-50",
         backgroundColor_Tag: "bg-yellow-100",
         backgroundColor_Card: "bg-yellow-200",
-        textColor: "text-yellow-950"
+        textColor: "text-yellow-950",
+        hexCode: "#422006",
     },
     {
         name: "Lime",
         backgroundColor: "bg-lime-50",
         backgroundColor_Tag: "bg-lime-100",
         backgroundColor_Card: "bg-lime-200",
-        textColor: "text-lime-950"
+        textColor: "text-lime-950",
+        hexCode: "#1a2e05",
     },
     {
         name: "Green",
         backgroundColor: "bg-green-50",
         backgroundColor_Tag: "bg-green-100",
         backgroundColor_Card: "bg-green-200",
-        textColor: "text-green-950"
+        textColor: "text-green-950",
+        hexCode: "#052e16",
     },
     {
         name: "Emerald",
         backgroundColor: "bg-emerald-50",
         backgroundColor_Tag: "bg-emerald-100",
         backgroundColor_Card: "bg-emerald-200",
-        textColor: "text-emerald-950"
+        textColor: "text-emerald-950",
+        hexCode: "#022c22",
     },
     {
         name: "Teal",
         backgroundColor: "bg-teal-50",
         backgroundColor_Tag: "bg-teal-100",
         backgroundColor_Card: "bg-teal-200",
-        textColor: "text-teal-950"
+        textColor: "text-teal-950",
+        hexCode: "#042f2e",
     },
     {
         name: "Cyan",
         backgroundColor: "bg-cyan-50",
         backgroundColor_Tag: "bg-cyan-100",
         backgroundColor_Card: "bg-cyan-200",
-        textColor: "text-cyan-950"
+        textColor: "text-cyan-950",
+        hexCode: "#083344",
     },
     {
         name: "Sky",
         backgroundColor: "bg-sky-50",
         backgroundColor_Tag: "bg-sky-100",
         backgroundColor_Card: "bg-sky-200",
-        textColor: "text-sky-950"
+        textColor: "text-sky-950",
+        hexCode: "#082f49",
     },
     {
         name: "Blue",
         backgroundColor: "bg-blue-50",
         backgroundColor_Tag: "bg-blue-100",
         backgroundColor_Card: "bg-blue-200",
-        textColor: "text-blue-950"
+        textColor: "text-blue-950",
+        hexCode: "#172554",
     },
     {
         name: "Indigo",
         backgroundColor: "bg-indigo-50",
         backgroundColor_Tag: "bg-indigo-100",
         backgroundColor_Card: "bg-indigo-200",
-        textColor: "text-indigo-950"
+        textColor: "text-indigo-950",
+        hexCode: "#1e1b4b",
     },
     {
         name: "Violet",
         backgroundColor: "bg-violet-50",
         backgroundColor_Tag: "bg-violet-100",
         backgroundColor_Card: "bg-violet-200",
-        textColor: "text-violet-950"
+        textColor: "text-violet-950",
+        hexCode: "#2e1065",
     },
     {
         name: "Purple",
         backgroundColor: "bg-purple-50",
         backgroundColor_Tag: "bg-purple-100",
         backgroundColor_Card: "bg-purple-200",
-        textColor: "text-purple-950"
+        textColor: "text-purple-950",
+        hexCode: "#3b0764",
     },
     {
         name: "Fuchsia",
         backgroundColor: "bg-fuchsia-50",
         backgroundColor_Tag: "bg-fuchsia-100",
         backgroundColor_Card: "bg-fuchsia-200",
-        textColor: "text-fuchsia-950"
+        textColor: "text-fuchsia-950",
+        hexCode: "#4a044e",
     },
     {
         name: "Pink",
         backgroundColor: "bg-pink-50",
         backgroundColor_Tag: "bg-pink-100",
         backgroundColor_Card: "bg-pink-200",
-        textColor: "text-pink-950"
+        textColor: "text-pink-950",
+        hexCode: "#500724",
     },
     {
         name: "Rose",
         backgroundColor: "bg-rose-50",
         backgroundColor_Tag: "bg-rose-100",
         backgroundColor_Card: "bg-rose-200",
-        textColor: "text-rose-950"
+        textColor: "text-rose-950",
+        hexCode: "#4c0519",
     }
 ];
 
